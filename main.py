d17144781beeb8bf3e4d179fd0d183f3d574ea5b
rana-akbar <rana.akbar.08@gmail.com>
2022-05-08 01:01:57 +0100

    user promts for input and, and print statement returning generated name, Project completed

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #1. Create a greeting for your program.
-
+print("Welcome to Band GEN v1 ! we will ask you a few questions to make you a cool Band name!!!\n")
 #2. Ask the user for the city that they grew up in.
-
+user_city = input("Please tell us the city you grew up in\n")
 #3. Ask the user for the name of a pet.
-
+pet_name = input("Thank you, for providing you city, and now please input your pets name\n")
 #4. Combine the name of their city and pet and show them their band name.
-
+print(user_city + pet_name)
 #5. Make sure the input cursor shows on a new line, see the example at:
 #   https://replit.com/@appbrewery/band-name-generator-end
